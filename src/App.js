476b90da387ed3a1d2bce6a0f5604ea7e2f79b1c
Mutahhir-Khan <63418479+Mutahhir-Khan@users.noreply.github.com>
2020-12-31 15:04:19 +0500

    done with rendering jumbotron

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Jumbotron from "./Components/Jumbotron/index";
+import jumboData from "./Fixtures/jumbo.json"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Jumbotron.Container>
+        {jumboData.map((item) => (
+          <Jumbotron key={item.id} direction={item.direction}>
+            {/* this gonna on left side */}
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle> {/* -->  same as passing the props */} 
+            </Jumbotron.Pane>
+            {/* this gonna be on right */}
+            <Jumbotron.Pane>
+              <Jumbotron.Image src={item.image} alt={item.alt} />
+            </Jumbotron.Pane>
+
+          </Jumbotron>
+        ))}
+      </Jumbotron.Container>
     </div>
   );
 }
